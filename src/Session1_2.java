efc3b28daa994e266b91e9d940463e86dea8615e
unknown <gi_berkane@esi.dz>
2021-03-25 00:26:20 +0100

    Session 1&2: Difference between imperative and declarative programming

---
+++ src/Session1_2.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Session1_2 {
+
+    private static List<Integer> myData=  Arrays.asList(7,4,5,6,3,8,10);
+    public static void main(String[] args) {
+
+        //imperative
+        System.out.println("imperative");
+        for (Integer value:myData
+             ) {
+            System.out.println(SubtractTen(Square(AddOne(value))).toString());
+        }
+
+        //declarative
+        System.out.println("declarative");
+        myData.stream()
+                .map(Session1_2::AddOne)
+                .map(Session1_2::Square)
+                .filter(value -> value<70)
+                .sorted(Comparator.comparingInt(value -> value))
+                .limit(2)
+                .map(Session1_2::SubtractTen)
+
+
+                .map(val ->{
+                    System.out.println(val.toString());
+                    return null;
+                }).collect(Collectors.toList());
+
+
+        //declarative using composition
+
+
+
+    }
+
+
+
+    private static Integer SubtractTen(Integer v){
+        return v-10;
+    }
+
+    private static Integer Square(Integer v){
+        return v*v;
+    }
+
+    private static Integer AddOne(Integer v){
+        return v+1;
+    }
+}
